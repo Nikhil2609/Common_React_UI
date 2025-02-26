7e573f558454b3d46021d4ed874feda90d19d836
Nikhil2609 <75295745+Nikhil2609@users.noreply.github.com>
2025-02-26 17:42:30 +0530

    Update CommonDialogue.tsx

--- CommonDialogue.tsx
+++ CommonDialogue.tsx
@@ -1,51 +1,122 @@
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@mui/material";
+import React from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  styled,
+  CircularProgress
+} from "@material-ui/core";
+import CommonButton from "./CommonButton.web";
 
-function ConfirmationDialogue(props: DialogueProps) {
-    const { open, body, title, confirmBtnText, confirmBtnClick, cancelBtnClick, cancelBtnText, closeDialogue } = props;
-    return (
-        <>
-            <Dialog open={open} onClose={() => closeDialogue()}>
-                <DialogTitle>{title}</DialogTitle>
-                <DialogContent>
-                    {body}
-                </DialogContent>
-                <DialogActions style={{ display: "flex", justifyContent: "center", marginTop: "16px" }}>
-                    <Button onClick={() => cancelBtnClick()} color="secondary" variant='outlined'>
-                        {cancelBtnText}
-                    </Button>
-                    <Button onClick={() => confirmBtnClick()} color="error" variant='contained'>
-                        {confirmBtnText}
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </>
-    );
+interface DialgoueProps {
+  open: boolean;
+  title: string;
+  body: React.ReactNode | string;
+  submitBtnText: string;
+  cancelBtnText?: string;
+  handleCancel: Function;
+  handleSubmit: Function;
+  isLoading?: boolean;
 }
 
-export default ConfirmationDialogue;
+const CommonDialogue = (props: DialgoueProps) => {
+  const {
+    open,
+    title,
+    body,
+    submitBtnText,
+    cancelBtnText,
+    handleSubmit,
+    handleCancel,
+    isLoading
+  } = props;
+  return (
+    <>
+      <StyledDialogue
+        data-test-id="dialogue"
+        maxWidth={"xs"}
+        open={open}
+        onClose={() => handleCancel()}
+      >
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>{body}</DialogContent>
+        <DialogActions>
+          {cancelBtnText && (
+            <Button
+              onClick={() => handleCancel()}
+              variant="outlined"
+              className="cancelBtn"
+            >
+              {cancelBtnText}
+            </Button>
+          )}
+          <CommonButton
+            onClick={() => handleSubmit()}
+            variant="primary"
+            className="submitBtn"
+            isLoading={isLoading} 
+            label={submitBtnText}
+            dataTestId={"commonDialogSubmitBtn"}
+          />
+        </DialogActions>
+      </StyledDialogue>
+    </>
+  );
+};
 
-interface DialogueProps {
-    open: boolean;
-    closeDialogue: Function;
-    title: string;
-    body: string;
-    cancelBtnText: string;
-    confirmBtnText: string;
-    cancelBtnClick: Function;
-    confirmBtnClick: Function;
-}
+const StyledDialogue = styled(Dialog)({
+  "& .MuiPaper-rounded": {
+    borderRadius: "32px"
+  },
+  "& .MuiDialogTitle-root": {
+    borderBottom: "1px solid #E2E8F0",
+  },
+  "& .MuiDialogContent-root": {
+    backgroundColor: "white",
+  },
+  "& .MuiDialogActions-root": {
+    display: "flex",
+    justifyContent: "space-between",
+    gap: "16px"
+  },
+  "& .MuiButton-root": {
+    padding: "10px 16px",
+    textTransform: "none",
+    width: "100%",
+  },
+  "& .cancelBtn": {
+    color: "#684EF3",
+    backgroundColor: "#FFFFFF",
+    border: "1px solid #684EF3",
+  },
+  "& .submitBtn": {
+    color: "#FFFFFF",
+    backgroundColor: "684EF3",
+  },
+});
 
-    
-        
-        // used when you require for confirmation dialogue
+export default CommonDialogue;
 
-        // <ConfirmationDialogue
-        //   open={deleteBookDialogue}
-        //   title='Delete Book'
-        //   body='Are you sure you want to delete this book?'
-        //   cancelBtnText='Cancel'
-        //   cancelBtnClick={() => toggleDeleteBookPrompt()}
-        //   confirmBtnText='Delete'
-        //   confirmBtnClick={() => handleDeleteBook()}
-        //   closeDialogue={() => toggleDeleteBookPrompt()}
-        // />
+
+// use of common dialogue
+<CommonDialogue 
+    open={this.state.openHelpSupportDialogue} 
+    title={"Help & support"} 
+    body={
+        <Typography style={{fontWeight: 400, fontSize: "16px", fontFamily: "DM Sans"}}>
+            <Typography style={{paddingBottom: "16px"}}> If you have any questions or run into issues, our support team is ready to assist you.</Typography>
+            You can reach us at <span style={{fontWeight: 700, fontSize: "16px"}}>support@yourapp.com</span>  for any inquiries or assistance. We typically respond within 24 hours.
+        </Typography>
+    } 
+    submitBtnText={"Got it!"} 
+    handleCancel={this.toggleHelpSupportDialogue} 
+    handleSubmit={this.toggleHelpSupportDialogue}                    
+/>
+
+toggleHelpSupportDialogue = () => {
+    this.setState({
+        openHelpSupportDialogue: !this.state.openHelpSupportDialogue, 
+    });
+};
